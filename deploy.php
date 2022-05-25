90cfff6b56fd79f06a4c20ab92558838bfd9eee8
Andrey Helldar <helldar@ai-rus.com>
2022-05-26 01:01:57 +0300

    Update deployer config

--- deploy.php
+++ deploy.php
@@ -1,26 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Deployer;
 
-require 'recipe/laravel.php';
-require 'contrib/php-fpm.php';
 require 'contrib/npm.php';
+require 'contrib/php-fpm.php';
+require 'contrib/telegram.php';
+require 'recipe/laravel.php';
 
 // Config
 
-set('application', 'TheDragonCode');
-set('repository', 'git@github.com:TheDragonCode/api.dragon-code.pro.git');
+set('application', 'The Dragon Code: Web Template');
+set('repository', 'git@github.com:TheDragonCode/web-app.git');
 set('php_fpm_version', '8.1');
+
+// Notification
+
+set('telegram_token', $_SERVER['TELEGRAM_DRAGON_BOT_TOKEN']);
+set('telegram_chat_id', $_SERVER['TELEGRAM_DRAGON_BOT_CHAT_ID']);
+
+set('telegram_text', 'Deploying `{{branch}}` to *{{target}}*' . PHP_EOL . PHP_EOL . '*Application*: {{application}}');
+set('telegram_success_text', 'Deployed some fresh code to *{{target}}*' . PHP_EOL . PHP_EOL . '*Application*: {{application}}');
+set('telegram_failure_text', 'Something went wrong during deployment to *{{target}}*' . PHP_EOL . PHP_EOL . '*Application*: {{application}}');
 
 // Hosts
 
 host('production')
-    ->setHostname('api.dragon-code.pro')
+    ->setHostname('example.com')
     ->setRemoteUser('forge')
     ->setDeployPath('~/{{hostname}}');
 
 host('staging')
-    ->setHostname('api-staging.dragon-code.pro')
+    ->setHostname('staging.example.com')
     ->setRemoteUser('forge')
     ->setDeployPath('~/{{hostname}}');
 
@@ -35,12 +47,12 @@ task('deploy', [
     'artisan:view:cache',
     'artisan:event:cache',
     'artisan:migrate',
-    'artisan:migrate:actions',
     'npm:install',
     'npm:run:prod',
     'deploy:publish',
     'php-fpm:reload',
     'artisan:queue:restart',
+    'artisan:migrate:actions',
 ]);
 
 task('artisan:migrate:actions', function () {
@@ -53,4 +65,9 @@ task('npm:run:prod', function () {
     run('npm run prod');
 });
 
+before('deploy', 'telegram:notify');
+
+after('deploy:success', 'telegram:notify:success');
+
 after('deploy:failed', 'deploy:unlock');
+after('deploy:failed', 'telegram:notify:failure');
